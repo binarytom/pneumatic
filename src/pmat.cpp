014040d748ec351d08a397803c119730e6a76e03
Tom Molesworth <TEAM@cpan.org>
2015-04-12 18:27:57 +0100

    Name lookup table

---
+++ src/pmat.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file pmat.cpp
+ * @author Tom Molesworth <tom@audioboundary.com>
+ * @date 08/04/15 22:10:50
+ *
+ * 
+ * $Id$
+ */
+
+
+std::string main_cv() const { return u8"the main code"; }
+std::string defstash() const { return u8"the default stash"; }
+std::string mainstack() const { return u8"the main stack AV"; }
+std::string beginav() const { return u8"the BEGIN list"; }
+std::string checkav() const { return u8"the CHECK list"; }
+std::string unitcheckav() const { return u8"the UNITCHECK list"; }
+std::string initav() const { return u8"the INIT list"; }
+std::string endav() const { return u8"the END list"; }
+std::string strtab() const { return u8"the shared string table HV"; }
+std::string envgv() const { return u8"the ENV GV"; }
+std::string incgv() const { return u8"the INC GV"; }
+std::string statgv() const { return u8"the stat GV"; }
+std::string statname() const { return u8"the statname SV"; }
+std::string tmpsv() const { return u8"the temporary SV"; }
+std::string defgv() const { return u8"the default GV"; }
+std::string argvgv() const { return u8"the ARGV GV"; }
+std::string argoutgv() const { return u8"the argvout GV"; }
+std::string argvout_stack() const { return u8"the argout stack AV"; }
+std::string fdpidav() const { return u8"the FD-to-PID mapping AV"; }
+std::string preambleav() const { return u8"the compiler preamble AV"; }
+std::string modglobalhv() const { return u8"the module data globals HV"; }
+std::string regex_padav() const { return u8"the REGEXP pad AV"; }
+std::string sortstash() const { return u8"the sort stash"; }
+std::string firstgv() const { return u8"the *a GV"; }
+std::string secondgv() const { return u8"the *b GV"; }
+std::string debstash() const { return u8"the debugger stash"; }
+std::string stashcache() const { return u8"the stash cache"; }
+std::string isarev() const { return u8"the reverse map of @ISA dependencies"; }
+std::string registered_mros() const { return u8"the registered MROs HV"; }
+std::string rs() const { return u8"the IRS"; }
+std::string last_in_gv() const { return u8"the last input GV"; }
+std::string ofsgv() const { return u8"the OFS GV"; }
+std::string defoutgv() const { return u8"the default output GV"; }
+std::string hintgv() const { return u8"the hints (%^H) GV"; }
+std::string patchlevel() const { return u8"the patch level"; }
+std::string apiversion() const { return u8"the API version"; }
+std::string e_script() const { return u8"the '-e' script"; }
+std::string mess_sv() const { return u8"the message SV"; }
+std::string ors_sv() const { return u8"the ORS SV"; }
+std::string encoding() const { return u8"the encoding"; }
+std::string blockhooks() const { return u8"the block hooks"; }
+std::string custom_ops() const { return u8"the custom ops HV"; }
+std::string custom_op_names() const { return u8"the custom op names HV"; }
+std::string custom_op_descs() const { return u8"the custom op descriptions HV"; }
+
