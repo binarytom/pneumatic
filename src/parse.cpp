f93761a8e3d048193e73bfa3c6dd56972256f740
Tom Molesworth <TEAM@cpan.org>
2015-04-15 08:12:44 +0100

    Trace is now controlled by commandline parameter

--- src/parse.cpp
+++ src/parse.cpp
@@ -5,14 +5,41 @@
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
 
 #include "detail.h"
 #include "Log.h"
 
 using namespace std;
 
-int main(int argc, char **argv) {
-	if(true) {
+int
+main(int argc, char **argv) {
+	namespace po = boost::program_options;
+
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help", u8"show program options")
+		("trace", po::value<bool>(), u8"excessive debug tracing output")
+	;
+
+	po::variables_map vm;
+	po::store(
+		po::parse_command_line(
+			argc, argv, desc
+		),
+		vm
+	);
+	po::notify(vm);    
+
+	if(vm.count("help")) {
+		cout << desc << "\n";
+		return 1;
+	}
+
+	if (vm.count("trace")) {
+	} else {
 		namespace logging = boost::log;
 		logging::core::get()->set_filter(
 			/* phoenix template thingey, comparison is stored not the result */
